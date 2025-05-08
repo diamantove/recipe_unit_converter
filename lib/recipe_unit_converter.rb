761299597754eeffcafa4e04494cc6570ce53ed2
Рустам Уджуху <diamantove@mail.ru>
2025-05-08 13:33:29 +0300

    updated structure

--- lib/recipe_unit_converter.rb
+++ lib/recipe_unit_converter.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "recipe_unit_converter/version"
+require_relative "recipe_unit_converter/converter"
+require_relative "recipe_unit_converter/extensions/numeric"
 
 module RecipeUnitConverter
   class Error < StandardError; end
-  # Your code goes here...
 end
